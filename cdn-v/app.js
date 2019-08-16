cc728edda252f81ed55c8b9eb9b63170003d867d
Chachay <t.sasai0802@gmail.com>
2019-08-17 00:36:09 +0900

    WIP

--- cdn-v/app.js
+++ cdn-v/app.js
@@ -2,30 +2,45 @@
 const app = new Vue({
   el: '#app',
   data: {
-    items: [
-      { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-      { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-      { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-      { age: 38, first_name: 'Jami', last_name: 'Carney' }
+    perPage: 25,
+    currentPage: 1,
+    header: ["CODE","TYPE","MATURITY","STRIKE", "RSV", 
+    "PUT_CODE", "PUT_PRICE", "PUT_RSV", "PUT_TPRICE", "PUT_VOLATILITY",
+    "CALL_CODE","CALL_PRICE","CALL_RSV","CALL_TPRICE","CALL_VOLATILITY",
+    "F225_PRICE", "Base_VOL"],
+    fields: ["MATURITY","STRIKE",  
+     "PUT_PRICE",  "PUT_TPRICE", "PUT_VOLATILITY",
+     "CALL_PRICE", "CALL_TPRICE","CALL_VOLATILITY",
     ],
+    items: [],
+    categories:[],
     t_items:{}
   },
   created() {
+    let tmp = Papa.parse(this.header.join(",") + "\n" + csv_string,{header:true}).data;
+    console.log(tmp[0]);
+    this.items = alasql('SELECT * FROM ? WHERE CODE="NK225E    "', [tmp]);
     this.t_items = Object.assign(
       ...Object
         .keys(this.items[0])
         .map( key => ({ [key]: this.items.map( o => o[key] ) }))
       );
+    this.categories = Array.from(new Set(this.t_items["MATURITY"]));
     console.log("created"+ this.t_items)
   },
   mounted: function() {
     Plotly.plot( tester, 
       [{
-        x: this.t_items['first_name'],
-        y: this.t_items['age']
+        x: this.t_items['STRIKE'],
+        y: this.t_items['PUT_TPRICE']
       }],
       {
         margin: { t: 0 } 
       });
+  },
+  computed: {
+    rows() {
+      return this.items.length
+    }
   }
 });
